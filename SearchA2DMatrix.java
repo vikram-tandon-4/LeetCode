1598a46a8dee098918bb272d3bfec31e775a7e2c
vikram-tandon-4 <vikramtandon4192@gmail.com>
2019-03-01 01:59:19 -0600

    Search a 2D matrix

---
+++ SearchA2DMatrix.java
@@ -0,0 +1,63 @@
+/*
+ Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+ */
+public class SearchA2DMatrix {
+
+	public static void main(String[] args) {
+		int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
+		System.out.println(searchMatrix(matrix, 13));
+	}
+
+	public static boolean searchMatrix(int[][] matrix, int target) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return false;
+		}
+		int numberOfColumns = matrix[0].length, numberOfRows = matrix.length;
+
+		int start = 0;
+		int end = (numberOfRows * numberOfColumns) - 1;
+		int mid = 0;
+		while (start <= end) {
+
+			mid = (start + end) / 2;
+			int[] pos = getPosition(mid, numberOfColumns);
+			System.out.println("number = " + matrix[pos[0]][pos[1]] + " at position = " + pos[0] + ", " + pos[1]);
+			if (target == matrix[pos[0]][pos[1]]) {
+				return true;
+			} else if (target < matrix[pos[0]][pos[1]]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return false;
+	}
+
+	public static int[] getPosition(int mid, int columns) {
+		int[] position = { mid / columns, mid % columns };
+		return position;
+	}
+}
